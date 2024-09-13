341057d643c250f8c0a14dfa4bb6e1d8ef76d529
Oussama <oussamay713@gmail.com>
2024-09-13 18:20:27 +0200

    Arithmetic operations of two numbers

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+#Perform arithmetic operations
+add = number1 + number2
+sub = number1 - number2
+Mult = number1 * number2
+
+print("Addition of",number1, "and" ,number2, "is",add)
+print("Subtraction of",number1,"and",number2,"is",sub)
+print("Multiplication of",number1,"and",number2,"is",Mult)
