551b4a08d7a80ed73003f7619d3ee0ee5df4f66e
Oussama <oussamay713@gmail.com>
2024-09-13 18:27:26 +0200

    Arithmetic operations of 2 nembers

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -5,6 +5,7 @@ add = number1 + number2
 sub = number1 - number2
 Mult = number1 * number2
 
+#Display results
 print("Addition of",number1, "and" ,number2, "is",add)
 print("Subtraction of",number1,"and",number2,"is",sub)
 print("Multiplication of",number1,"and",number2,"is",Mult)
