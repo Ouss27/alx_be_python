e85516b7affdc2f014bcc18a3be35425a66ac24c
Oussama <oussamay713@gmail.com>
2024-09-14 17:52:40 +0200

    Age calculator in a specific year

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,7 @@
+age = input("How old are you ? ") 
+#assume the user will input a valid integer value
+
+age_by_2050 = int(age) + 27
+#assume the current year is 2023
+
+print("In 2050, you will be", age_by_2050, "years old.")
