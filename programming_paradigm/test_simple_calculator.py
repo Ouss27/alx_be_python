2efc220acf5a3ac69ed7ee5006e1095cd011e2c2
Oussama <oussamay713@gmail.com>
2024-10-05 18:00:49 +0200

    test simple calculator v0

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,35 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class Test(unittest.TestCase):
+
+    #Set up a SimpleCalculator instance for use in all tests
+    def set_up(self):
+        self.calc = SimpleCalculator
+
+    #test addition method
+    def test_add(self):
+        self.assertAlmostEqual(self.calc.add(1,2),3)
+        self.assertAlmostEqual(self.calc.add(1,-3),-2)
+        self.assertAlmostEqual(self.calc.add(0,0),0)
+
+    #test substraction method
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(0, 5), -5)
+        self.assertEqual(self.calc.subtract(-5, -5), 0)
+
+    #test multiplication method
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(0, 3), 0)
+        self.assertEqual(self.calc.multiply(-1, -1), 1)
+
+    #test division method
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(3, 1), 3)
+        self.assertEqual(self.calc.divide(5, 0), None)  # Division by zero
+
+if __name__ == '__main__':
+    unittest.main()
